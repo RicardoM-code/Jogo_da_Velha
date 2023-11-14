915777f3745d37b267e233f0bd6f489f94539f5e
RicardoM-code <ricardomagns.rm@gmail.com>
2023-11-14 15:53:51 -0300

    Jogo da Velha

---
+++ scripts/interface.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick)
+    })
+
+})
+
+function handleClick(event) {
+
+
+    let square = event.target;
+    let postion = square.id;
+
+    if (handleMove(postion)) {
+
+        setTimeout(() => {
+            alert(" O Jogo Acabou - O Vencedor foi " + playerTime);
+        }, 10);
+
+    };
+    updateSquare(postion);
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML = `<div class ='${symbol}'></div> `
+}
+
